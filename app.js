3975b52eddafd1216f9e914da8c9b630c18b0394
Gabriel La Tore <42503800+GabrielLaTorre@users.noreply.github.com>
2020-02-20 21:47:27 -0300

    Programa de operaciones matemáticas

---
+++ app.js
@@ -0,0 +1,48 @@
+var password = "estrellita123";
+var input = prompt("Ingrese su clave: ");
+
+function suma() {
+  var valor1 = parseInt(prompt("Ingrese el primer valor #1"));
+  var valor2 = parseInt(prompt("Ingrese el segundo valor #2"));
+  var resultado = valor1 + valor2;
+  return resultado;
+}
+
+function promedio() {
+  var valor1 = parseInt(prompt("Ingrese el primer valor #1"));
+  var valor2 = parseInt(prompt("Ingrese el segundo valor #2"));
+  var valor3 = parseInt(prompt("Ingrese el primer valor #3"));
+  var valor4 = parseInt(prompt("Ingrese el segundo valor #4"));
+  var valor5 = parseInt(prompt("Ingrese el primer valor #5"));
+  var resultado = (valor1 + valor2 + valor3 + valor4 + valor5) / 5;
+
+  return resultado;
+}
+
+function perimetro() {
+  var diametro = parseInt(prompt("Ingrese el diametro"));
+  var resultado = diametro * Math.PI;
+
+  return resultado;
+}
+
+if (input == password) {
+  alert("Operaciones disponibles: SUMA | PROMEDIO | PERIMETRO");
+
+  var operacion = prompt("Que operacion desea realizar");
+
+  if (operacion == "suma") {
+    var resultado_suma = suma();
+    alert(resultado_suma);
+  } else if (operacion == "promedio") {
+    var resultado_promedio = promedio();
+    alert(resultado_promedio);
+  } else if (operacion == "perimetro") {
+    var resultado_perimetro = perimetro();
+    alert(resultado_perimetro);
+  } else {
+    alert("Operacion no disponible");
+  }
+} else {
+  alert("Clave incorrecta (IP BLOQUEADA)");
+}
